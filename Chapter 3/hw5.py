c4d490f1c411ff4a81ced39a3c97735a4ce88d4e
mikkiduan <32303254+mikkiduan@users.noreply.github.com>
2017-10-02 20:25:34 -0400

    chap 3 hw

---
+++ Chapter 3/hw5.py
@@ -0,0 +1,19 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+tess=turtle.Turtle()
+tess.shape("turtle")
+tess.color("blue")
+tess.stamp()
+
+for i in range(12):
+    tess.penup()
+    tess.forward(100)
+    tess.pendown()
+    tess.forward(10)
+    tess.penup()
+    tess.forward(10)
+    tess.stamp()
+    tess.backward(120)
+    tess.left(30)
+    
---
+++ Chapter 3/turtle clock.py
@@ -0,0 +1,6 @@
+import turtle
+wn = turtle.Screnn()
+wn.bgcolor("lightgreen")
+tess = turtle .Tutrtle()
+tess.shape("turtle")
+tess.color("blue")
